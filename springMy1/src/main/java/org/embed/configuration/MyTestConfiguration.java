a989551e8b30f1bded5240e8349dc5b6b5f8aa57
user <user@DESKTOP-F06EBS7>
2025-10-23 12:11:00 +0900

    Web Hong ver 1.0

---
+++ springMy1/src/main/java/org/embed/configuration/MyTestConfiguration.java
@@ -0,0 +1,8 @@
+package org.embed.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyTestConfiguration {
+	String str = "여기는 홍 입니다.";
+}
